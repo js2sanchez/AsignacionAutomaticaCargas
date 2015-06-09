931358bf5dd3c2ca19d543520d4b0dfee79a4440
Rebeca Moreno <rbma91@gmail.com>
2015-06-08 19:28:54 -0500

    Cree una carpeta nueva que se llama Asignacion, para meter ahi lo q generemos. Consultar Asignacion no tiene nada  todavia

---
+++ ITCR.AsignacionAutomaticaCargas/ITCR.AsignacionAutomaticaCargas.Interfaz/View/Asignacion/Consultar_Asignacion.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ITCR.AsignacionAutomaticaCargas.Interfaz.View.Asignacion
+{
+    public partial class Consultar_Asignacion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
